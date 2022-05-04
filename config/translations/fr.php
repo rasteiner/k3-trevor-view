e4f7b269e8d16ae7b5b1d14075eb3e9faac7611e
Philippe Gervaise <github@malvese.org>
2022-05-04 09:49:42 +0200

    Create fr.php
    French translation

---
+++ config/translations/fr.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'trevor.heading' => 'Traductions',
+    'trevor.startinfo' => 'Choisir une variable à gauche pour commencer à traduire.',
+    'trevor.addkey' => 'Entrer un nom de variable',
+    'trevor.payoff' => 'nul ne tradouit mieux les voriables.'
+];
